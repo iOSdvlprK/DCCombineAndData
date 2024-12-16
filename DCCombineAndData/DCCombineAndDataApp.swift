0e60c2b3514de0924e383230a1c2699b72ab8f39
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-12-16 21:17:23 +0900

    Initial Commit

---
+++ DCCombineAndData/DCCombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCCombineAndDataApp.swift
+//  DCCombineAndData
+//
+//  Created by joe on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DCCombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
